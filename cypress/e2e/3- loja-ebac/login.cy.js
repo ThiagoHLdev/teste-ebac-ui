6fbba72ca764111459cdd603084d0d0a245a1b7c
Thiago Henrique <goncalvesthiago1132@gmail.com>
2025-04-20 12:27:40 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3- loja-ebac/login.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+describe('funcionalidade: login', () => {
+
+   it('deve fazer login com sucesso', () =>{
+       cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       cy.get('#username').type('thiago.teste@gmail.com.br')
+       cy.get('#password').type('@teste1234')
+       cy.get('.woocommerce-form > .button').click()
+
+       cy.get('.woocommerce-MyAccount-content > :nth-child(2)') .should('contain', 'Olá');
+
+
+   })
+
+})
